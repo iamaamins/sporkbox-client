c56726ecd9c7e8065094fc0b80eb0a3a8418626a
Alamin Shaikh <alaminshaikh@Alamins-MacBook-Pro.local>
2022-09-26 13:22:35 +0600

    Admin login created

---
+++ components/admin/login/ContactForm.tsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import { ChangeEvent, FormEvent } from "types";
+import axios from "axios";
+import styles from "@styles/admin/login/ContactForm.module.css";
+import { API_URL } from "@utils/index";
+
+export default function ContactForm() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [disabled, setDisabled] = useState(true);
+  const [loading, setLoading] = useState(false);
+
+  // Destructure form data and check
+  // If there is an empty field
+  const { email, password } = formData;
+  const hasEmpty = Object.values(formData).some((data) => data === "");
+
+  // Handle change
+  function handleChange(e: ChangeEvent) {
+    if (!hasEmpty) {
+      setDisabled(false);
+    }
+
+    setFormData((prevData) => ({
+      ...prevData,
+      [e.target.id]: e.target.value,
+    }));
+  }
+
+  // Handle submit
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post(`${API_URL}/admin/login`, formData);
+
+      console.log(res.data);
+
+      //   setFormData({
+      //     email: "",
+      //     password: "",
+      //   });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <section className={styles.contact_form}>
+      <p className={styles.title}>Sign in to your account</p>
+      <form onSubmit={handleSubmit}>
+        <div className={styles.item}>
+          <label htmlFor="email">Your email</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className={styles.item}>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handleChange}
+          />
+        </div>
+
+        <button
+          type="submit"
+          className={`${styles.button} ${!disabled && styles.active}`}
+        >
+          Sign in
+        </button>
+      </form>
+    </section>
+  );
+}
---
+++ pages/admin/login.tsx
@@ -0,0 +1,9 @@
+import ContactForm from "@components/admin/login/ContactForm";
+
+export default function LoginPage() {
+  return (
+    <main>
+      <ContactForm />
+    </main>
+  );
+}
