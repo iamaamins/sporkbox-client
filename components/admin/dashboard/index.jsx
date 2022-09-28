608da4fe9673602be7f47bffa9af4f8fd06cb4af
Alamin Shaikh <alaminshaikh@Alamins-MacBook-Pro.local>
2022-09-28 18:19:56 +0600

    Dashboard fine-tuned

--- components/admin/dashboard/index.jsx
+++ components/admin/dashboard/index.jsx
@@ -10,9 +10,9 @@ export default function Dashboard() {
 
         <div className={`${styles.title} ${styles.orders_title}`}>
           <p>Order</p>
-          <p>Date</p>
+          <p className={styles.hide_on_mobile}>Date</p>
           <p>Status</p>
-          <p>Total</p>
+          <p className={styles.hide_on_mobile}>Total</p>
           <p>Restaurant</p>
         </div>
 
@@ -20,7 +20,9 @@ export default function Dashboard() {
           <Link href={`/admin/restaurants/${createSlug("restaurant-name")}`}>
             <a className={styles.order}>
               <p>Order 1</p>
-              <p>Processing</p>
+              <p className={styles.hide_on_mobile}>Sep 28, 2022</p>
+              <p className={styles.gray}>Processing</p>
+              <p className={styles.hide_on_mobile}>$140</p>
               <p>Restaurant 1</p>
             </a>
           </Link>
@@ -28,7 +30,9 @@ export default function Dashboard() {
           <Link href={`/admin/restaurants/${createSlug("restaurant-name")}`}>
             <a className={styles.order}>
               <p>Order 2</p>
+              <p className={styles.hide_on_mobile}>Sep 30, 2022</p>
               <p>Processing</p>
+              <p className={styles.hide_on_mobile}>$180</p>
               <p>Restaurant 2</p>
             </a>
           </Link>
@@ -40,8 +44,8 @@ export default function Dashboard() {
 
         <div className={`${styles.title} ${styles.restaurants_title}`}>
           <p>Name</p>
-          <p>Email</p>
-          <p>Registered</p>
+          <p className={styles.hide_on_mobile}>Email</p>
+          <p className={styles.hide_on_mobile}>Registered</p>
           <p>Status</p>
         </div>
 
@@ -49,6 +53,8 @@ export default function Dashboard() {
           <Link href={`/admin/restaurants/${createSlug("restaurant-name")}`}>
             <a className={styles.restaurant}>
               <p>Restaurant 1</p>
+              <p className={styles.hide_on_mobile}>hello@restaurant1.com</p>
+              <p className={styles.hide_on_mobile}>Jun 12, 2022</p>
               <p>Active</p>
             </a>
           </Link>
@@ -56,6 +62,8 @@ export default function Dashboard() {
           <Link href={`/admin/restaurants/${createSlug("restaurant-name")}`}>
             <a className={styles.restaurant}>
               <p>Restaurant 2</p>
+              <p className={styles.hide_on_mobile}>hello@restaurant2.com</p>
+              <p className={styles.hide_on_mobile}>Aug 25, 2022</p>
               <p>Active</p>
             </a>
           </Link>
@@ -71,6 +79,7 @@ export default function Dashboard() {
 
         <div className={`${styles.title} ${styles.companies_title}`}>
           <p>Name</p>
+          <p className={styles.hide_on_mobile}>Code</p>
           <p>Budget</p>
         </div>
 
@@ -78,6 +87,7 @@ export default function Dashboard() {
           <Link href={`/admin/restaurants/${createSlug("restaurant-name")}`}>
             <a className={styles.company}>
               <p>Company 1</p>
+              <p className={styles.hide_on_mobile}>company1</p>
               <p>$140</p>
             </a>
           </Link>
@@ -85,6 +95,7 @@ export default function Dashboard() {
           <Link href={`/admin/restaurants/${createSlug("restaurant-name")}`}>
             <a className={styles.order}>
               <p>Company 2</p>
+              <p className={styles.hide_on_mobile}>company2</p>
               <p>$180</p>
             </a>
           </Link>
