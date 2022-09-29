b80d511605d850d982ab7c9d9d80d515cc17dbf1
Alamin Shaikh <alaminshaikh@Alamins-MacBook-Pro.local>
2022-09-29 11:40:03 +0600

    Order page created

--- components/admin/Order.jsx
+++ components/admin/Order.jsx
@@ -1,5 +1,47 @@
 import styles from "@styles/admin/Order.module.css";
 
 export default function Order() {
-  return <section className={styles.order}>Order</section>;
+  return (
+    <section className={styles.order}>
+      <div className={styles.details}>
+        <div className={styles.order_details}>
+          <h2>Order #1</h2>
+          <p>
+            <span>Date:</span> Sep 28, 2022
+          </p>
+          <p>
+            <span>Status:</span> Processing
+          </p>
+
+          <p>
+            <span>Total:</span> $140
+          </p>
+          <p>
+            <span>Items:</span> Item 1, Item 2
+          </p>
+          <p>
+            <span>Restaurant:</span> Restaurant 1
+          </p>
+        </div>
+
+        <div className={styles.customer_details}>
+          <h2>Customer info</h2>
+          <p>
+            <span>Name:</span> Customer 1
+          </p>
+          <p>
+            <span>Address:</span> Test address
+          </p>
+          <p>
+            <span>Phone:</span> 123-345-5555
+          </p>
+          <p>
+            <span>Email:</span> customer1@test.com
+          </p>
+        </div>
+      </div>
+
+      <button>Update status</button>
+    </section>
+  );
 }
