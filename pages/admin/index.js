5453fc0ba5384a2f6091f79a2a856b5056acf6ca
Alamin Shaikh <alaminshaikh@Alamins-MacBook-Pro.local>
2022-09-30 09:05:17 +0600

    Admin login page fine-tuned

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import { useLoader } from "@context/loader";
 import { useUser } from "@context/user";
 import { useRouter } from "next/router";
 import LoginForm from "@components/admin/LoginForm";
@@ -7,7 +6,6 @@ import LoginForm from "@components/admin/LoginForm";
 export default function LoginPage() {
   const router = useRouter();
   const { isAdmin } = useUser();
-  const { isLoading } = useLoader();
 
   // If not loading and admin
   // then push to the dashboard
@@ -15,12 +13,11 @@ export default function LoginPage() {
     if (isAdmin) {
       router.push("/admin/dashboard");
     }
-  }, [isLoading, isAdmin]);
+  }, [isAdmin]);
 
   return (
     <main>
-      {isLoading && <h1>Loading...</h1>}
-      {!isLoading && <LoginForm />}
+      <LoginForm />
     </main>
   );
 }
