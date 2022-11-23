2c2f9cb8cee998a7cd7f17460674d24bf712498c
Alamin Shaikh <hello@octib.com>
2022-11-23 12:06:16 +0600

    Order columns rearranged

--- components/generic/Orders.tsx
+++ components/generic/Orders.tsx
@@ -8,24 +8,24 @@ export default function Orders({ orders }: ICustomerOrderProps) {
     <table className={styles.orders}>
       <thead>
         <tr>
-          <th>Item</th>
-          <th className={styles.hide_on_mobile}>Quantity</th>
-          <th className={styles.hide_on_mobile}>Restaurant</th>
           <th>Delivery date</th>
+          <th className={styles.hide_on_mobile}>Restaurant</th>
+          <th>Item</th>
+          <th>Quantity</th>
         </tr>
       </thead>
 
       <tbody>
         {orders.map((order) => (
           <tr key={order._id}>
+            <td>{convertDateToText(order.deliveryDate)}</td>
+            <td className={styles.hide_on_mobile}>{order.restaurantName}</td>
             <td className={styles.important}>
               <Link href={`/dashboard/${order._id}`}>
                 <a>{order.item.name}</a>
               </Link>
             </td>
-            <td className={styles.hide_on_mobile}>{order.item.quantity}</td>
-            <td className={styles.hide_on_mobile}>{order.restaurantName}</td>
-            <td>{convertDateToText(order.deliveryDate)}</td>
+            <td>{order.item.quantity}</td>
           </tr>
         ))}
       </tbody>
