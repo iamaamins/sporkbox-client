3065c23bf0e243e0705c1fc2ec790b51600a4bbc
Alamin Shaikh <hello@octib.com>
2023-01-10 13:26:09 +0600

    Restricted access to the cart page

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,9 +1,22 @@
 import Cart from "@components/generic/Cart";
+import PageLoader from "@components/layout/PageLoader";
+import { useUser } from "@context/User";
+import { checkUser } from "@utils/index";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function CartPage() {
+  const router = useRouter();
+  const { isUserLoading, isCustomer } = useUser();
+
+  useEffect(() => {
+    checkUser(isUserLoading, isCustomer, router);
+  }, [isUserLoading, isCustomer]);
+
   return (
     <main>
-      <Cart />
+      {isUserLoading && <PageLoader />}
+      {isCustomer && <Cart />}
     </main>
   );
 }
