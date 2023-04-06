ae8ced1e2e55cb2683afa9c0946bf1b3aeb0b595
Alamin Shaikh <hello@octib.com>
2023-04-06 11:54:26 +0600

    Instructions added on the profile page

--- components/generic/Profile.tsx
+++ components/generic/Profile.tsx
@@ -4,6 +4,7 @@ import ShiftChangeModal from "./ShiftChangeModal";
 import { formatCurrencyToUSD } from "@utils/index";
 import styles from "@styles/generic/Profile.module.css";
 import ModalContainer from "@components/layout/ModalContainer";
+import LinkButton from "@components/layout/LinkButton";
 
 export default function Profile() {
   // Hooks
@@ -52,11 +53,88 @@ export default function Profile() {
 
           {customer.shifts.length > 0 && (
             <button onClick={() => setShowShiftChangeModal(true)}>
-              Change shift
+              Select shift
             </button>
           )}
         </div>
       )}
+
+      <div className={styles.instructions}>
+        <p className={styles.title}>Getting started with Spork Box</p>
+
+        <div className={styles.instruction}>
+          <p>
+            <strong>Step 1</strong>: Choose a shift
+          </p>
+          <ul>
+            <li>
+              Click the 'Select shift' button and select the shift you currently
+              work on.
+            </li>
+            <li>
+              The details of your shift will display above the 'Change shift'
+              button.
+            </li>
+          </ul>
+        </div>
+
+        <div className={styles.instruction}>
+          <p>
+            <strong>Step 2</strong>: Place your lunch orders
+          </p>
+          <ul>
+            <li>Click the 'Place order' tab on the menu bar above.</li>
+            <li>
+              You'll see all of the upcoming days that have scheduled
+              restaurants for ordering.
+            </li>
+            <li>
+              Your daily budget is how much your company will cover for you each
+              day. Any daily total over that amount, you'll need to cover at
+              check out.
+            </li>
+            <li>Prices includes all fees and gratuity.</li>
+          </ul>
+        </div>
+
+        <div className={styles.instruction}>
+          <p>
+            <strong>Step 3</strong>: Check out
+          </p>
+          <ul>
+            <li>
+              Make all of your selections for the week or time frame that you'll
+              be at the office.
+            </li>
+            <li>
+              Once you've made all of your selections, click on the bag logo at
+              the top right to see your cart.
+            </li>
+            <li>
+              If the cart looks good, click 'Check out'. If any days you've
+              order for exceed your company's budget, you'll need to check out
+              fo the remainder with a credit card.
+            </li>
+            <li>
+              Once you've checkout out, your 'Dashboard' tab on the menu will
+              show all of your upcoming and delivered orders.
+            </li>
+            <li>
+              If you need to modify an order, contact{" "}
+              <a href="mailto: portland@sporkbytes.com">
+                portland@sporkbytes.com
+              </a>{" "}
+              as soon as possible.
+            </li>
+          </ul>
+        </div>
+
+        <a href="/" target="_blank" className={styles.video_link}>
+          Watch a video on how Spork Box works here!{" "}
+        </a>
+
+        <LinkButton linkText="Leave feedback" href="/leave-feedback" />
+      </div>
 
       <ModalContainer
         showModalContainer={showShiftChangeModal}
