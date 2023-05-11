18058f730bbd7229094947c908e8d6ad0dff4028
Alamin Shaikh <hello@octib.com>
2023-05-11 12:42:30 +0600

    Made calendar filter persist

--- components/generic/CalendarFiltersModal.tsx
+++ components/generic/CalendarFiltersModal.tsx
@@ -1,6 +1,8 @@
-import { tags } from "@utils/index";
+import { useUser } from "@context/User";
+import { useRouter } from "next/router";
 import { ICalendarFiltersProps } from "types";
-import { ChangeEvent, FormEvent, useState } from "react";
+import { tags as tagsFilters } from "@utils/index";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import styles from "@styles/generic/CalendarFiltersModal.module.css";
 
 export default function CalendarFiltersModal({
@@ -8,44 +10,73 @@ export default function CalendarFiltersModal({
   setUpdatedRestaurants,
   setShowCalendarFilters,
 }: ICalendarFiltersProps) {
-  // Hooks
-  const [twentyAndUnder, setTwentyAndUnder] = useState(false);
-  const [tagsData, setTagsData] = useState(
-    tags.reduce((acc, curr) => ({ ...acc, [curr.toLowerCase()]: false }), {})
+  // All filters
+  const additionalFilters = ["$20 and under"];
+  const allFilters = [...tagsFilters, ...additionalFilters];
+
+  // Initial filters
+  const initialFilters = allFilters.reduce(
+    (acc, curr) => ({ ...acc, [curr.toLowerCase()]: false }),
+    {}
   );
 
+  // Hooks
+  const router = useRouter();
+  const { customer } = useUser();
+  const [filtersData, setFiltersData] = useState(initialFilters);
+
+  // Filter items
+  useEffect(() => {
+    filterItems();
+  }, [restaurants]);
+
+  useEffect(() => {
+    // Get filters data from local storage
+    const savedFilters = JSON.parse(
+      localStorage.getItem(
+        `filters-${customer?._id}-${router.query.date}`
+      ) as string
+    );
+
+    // Update state
+    setFiltersData(savedFilters || initialFilters);
+  }, [customer, router]);
+
   // Handle tags change
-  function handleTagsChange(e: ChangeEvent<HTMLInputElement>) {
-    setTagsData((currState) => ({
+  function handleFilterChange(e: ChangeEvent<HTMLInputElement>) {
+    setFiltersData((currState) => ({
       ...currState,
       [e.target.id.toLowerCase()]: e.target.checked,
     }));
   }
 
   // Filter items by tags
-  function filterItemsByTags(e: FormEvent) {
-    e.preventDefault();
-
+  function filterItems() {
     // Create updated restaurants
     let updatedRestaurants = restaurants;
 
-    // Create tags array
-    const tags = Object.entries(tagsData)
+    // All filters array
+    const allFilters = Object.entries(filtersData)
       .filter((data) => data[1] === true)
       .map((data) => data[0]);
 
+    // Only keep tag filters
+    const tagsFilters = allFilters.filter(
+      (filter) => !additionalFilters.includes(filter)
+    );
+
     // Filter items by dietary tags
-    if (tags.length > 0) {
+    if (tagsFilters.length > 0) {
       updatedRestaurants = updatedRestaurants.map((updatedRestaurant) => ({
         ...updatedRestaurant,
         items: updatedRestaurant.items.filter((item) =>
-          tags.some((tag) => item.tags.toLowerCase().includes(tag))
+          tagsFilters.some((tag) => item.tags.toLowerCase().includes(tag))
         ),
       }));
     }
 
     // Filter items with price $20 and less
-    if (twentyAndUnder) {
+    if (allFilters.includes("$20 and under")) {
       updatedRestaurants = updatedRestaurants.map((updatedRestaurant) => ({
         ...updatedRestaurant,
         items: updatedRestaurant.items.filter((item) => item.price <= 20),
@@ -53,7 +84,6 @@ export default function CalendarFiltersModal({
     }
 
     // Update states
-    setShowCalendarFilters(false);
     setUpdatedRestaurants(
       updatedRestaurants.filter(
         (updatedRestaurant) => updatedRestaurant.items.length > 0
@@ -61,32 +91,38 @@ export default function CalendarFiltersModal({
     );
   }
 
+  // Filter items by tags
+  function filterItemsOnSubmit(e: FormEvent) {
+    // Prevent default
+    e.preventDefault();
+
+    // Filter items and remove modal
+    filterItems();
+    setShowCalendarFilters(false);
+
+    // Save filters data to local storage
+    localStorage.setItem(
+      `filters-${customer?._id}-${router.query.date}`,
+      JSON.stringify(filtersData)
+    );
+  }
+
   return (
     <div className={styles.calendar_filters_modal}>
       <h2>Filters</h2>
 
-      <form onSubmit={filterItemsByTags}>
-        {tags.map((tag, index) => (
+      <form onSubmit={filterItemsOnSubmit}>
+        {allFilters.map((filter, index) => (
           <div key={index} className={styles.tag}>
             <input
               type="checkbox"
-              id={tag}
-              onChange={handleTagsChange}
-              checked={tagsData[tag.toLowerCase() as keyof object]}
+              id={filter}
+              onChange={handleFilterChange}
+              checked={filtersData[filter.toLowerCase() as keyof object]}
             />
-            <label htmlFor={tag}>{tag}</label>
+            <label htmlFor={filter}>{filter}</label>
           </div>
         ))}
-
-        <div className={styles.twenty_and_under}>
-          <input
-            type="checkbox"
-            id="twentyAndUnder"
-            checked={twentyAndUnder}
-            onChange={(e) => setTwentyAndUnder(e.target.checked)}
-          />
-          <label htmlFor="twentyAndUnder">$20 and under</label>
-        </div>
 
         <input type="submit" value="Apply" />
       </form>
