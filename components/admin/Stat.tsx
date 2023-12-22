a6bd7e695c71ae44ec11dd130e8f99e45a24d52f
Alamin Shaikh <hello@octib.com>
2023-12-22 17:21:07 +0600

    Error alert added to stat

--- components/admin/Stat.tsx
+++ components/admin/Stat.tsx
@@ -4,34 +4,43 @@ import {
   itemStatCSVHeaders,
   orderStatCSVHeaders,
 } from '@utils/csv';
-import { axiosInstance } from '@utils/index';
+import { axiosInstance, showErrorAlert } from '@utils/index';
 import { useEffect, useState } from 'react';
 import { CSVLink } from 'react-csv';
 import { FiDownload } from 'react-icons/fi';
 import styles from '@styles/admin/Stat.module.css';
+import { CustomAxiosError } from 'types';
+import { useAlert } from '@context/Alert';
 
 export default function Stat() {
   // Hook
+  const { setAlerts } = useAlert();
   const [orderStat, setOrderStat] = useState([]);
   const [itemStat, setItemStat] = useState([]);
 
   // Download order stat
   async function downloadOrderStat() {
     try {
+      // Get order stat and update state
       const response = await axiosInstance.get('/stats/order');
       setOrderStat(response.data);
     } catch (err) {
+      // Log error
       console.log(err);
+      showErrorAlert(err as CustomAxiosError, setAlerts);
     }
   }
 
   // Download item stat
   async function downloadItemStat() {
     try {
+      // Get item stat and update state
       const response = await axiosInstance.get('/stats/item');
       setItemStat(response.data);
     } catch (err) {
+      // Log error
       console.log(err);
+      showErrorAlert(err as CustomAxiosError, setAlerts);
     }
   }
 
@@ -42,25 +51,33 @@ export default function Stat() {
   }, []);
   return (
     <section className={styles.section}>
-      <div className={styles.item}>
-        <CSVLink
-          data={formatOrderStatToCSV(orderStat)}
-          headers={orderStatCSVHeaders}
-          filename='Order Stat'
-        >
-          Order stat <FiDownload />
-        </CSVLink>
-      </div>
+      {orderStat.length > 0 ? (
+        <div className={styles.item}>
+          <CSVLink
+            data={formatOrderStatToCSV(orderStat)}
+            headers={orderStatCSVHeaders}
+            filename='Order Stat'
+          >
+            Order stat <FiDownload />
+          </CSVLink>
+        </div>
+      ) : (
+        <p>No order stat found</p>
+      )}
 
-      <div className={styles.item}>
-        <CSVLink
-          data={formatItemStatToCSV(itemStat)}
-          headers={itemStatCSVHeaders}
-          filename='Item Stat'
-        >
-          Item stat <FiDownload />
-        </CSVLink>
-      </div>
+      {itemStat.length > 0 ? (
+        <div className={styles.item}>
+          <CSVLink
+            data={formatItemStatToCSV(itemStat)}
+            headers={itemStatCSVHeaders}
+            filename='Item Stat'
+          >
+            Item stat <FiDownload />
+          </CSVLink>
+        </div>
+      ) : (
+        <p>No item stat found</p>
+      )}
     </section>
   );
 }
