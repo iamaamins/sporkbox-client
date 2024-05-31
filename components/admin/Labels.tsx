cb9d5c0cb087c4a7374400a4a80a04bc441139f6
Alamin Shaikh <hello@octib.com>
2024-05-31 09:26:41 +0600

    Added label height and created dynamic font size calculation

--- components/admin/Labels.tsx
+++ components/admin/Labels.tsx
@@ -38,6 +38,7 @@ const styles = StyleSheet.create({
   },
   label: {
     width: 188,
+    height: 72,
     flexDirection: 'row',
     alignItems: 'center',
     marginVertical: 13,
@@ -49,9 +50,8 @@ const styles = StyleSheet.create({
     marginRight: 5,
   },
   name_shift: {
-    flexWrap: 'wrap',
-    flexDirection: 'row',
     marginBottom: 10,
+    flexDirection: 'row',
   },
   line: {
     width: 151,
@@ -70,35 +70,133 @@ type Props = {
 };
 
 export default function Labels({ orders }: Props) {
+  function calculateFontSize(
+    text: string,
+    maxWidth: number,
+    baseFontSize = 10
+  ) {
+    const charWidth = baseFontSize * 0.6;
+    const textWidth = text.length * charWidth;
+    if (textWidth <= maxWidth) return baseFontSize;
+    return maxWidth / text.length / 0.6;
+  }
+
   return (
     <Document>
       <Page size='LETTER' style={styles.page}>
-        {orders.map((order) => (
-          <View key={order._id} style={styles.label} wrap={false}>
-            <Image src='/label-icon.png' style={styles.logo} />
-            <View>
-              <View style={[styles.name_shift, styles.line]}>
-                <Text style={styles.bold}>{order.customer.firstName} </Text>
-                <Text>{order.customer.lastName} - </Text>
-                <Text style={[styles.capitalize, styles.bold]}>
-                  {order.company.shift === 'night'
-                    ? order.company.shift
-                    : categorizeLastName(order.customer.lastName)}
+        {orders.map((order) => {
+          const nameShift = `${order.customer.firstName} test case name ${
+            order.customer.lastName
+          } - ${
+            order.company.shift === 'night'
+              ? order.company.shift
+              : categorizeLastName(order.customer.lastName)
+          }`;
+          const addons = `${order.item.optionalAddons} ${order.item.requiredAddons}`;
+
+          return (
+            <View key={order._id} style={styles.label} wrap={false}>
+              <Image src='/label-icon.png' style={styles.logo} />
+              <View>
+                <View style={[styles.name_shift, styles.line]}>
+                  <Text
+                    style={[
+                      styles.bold,
+                      {
+                        fontSize: calculateFontSize(
+                          nameShift,
+                          styles.line.width
+                        ),
+                      },
+                    ]}
+                  >
+                    {order.customer.firstName} test case name{' '}
+                  </Text>
+                  <Text
+                    style={{
+                      fontSize: calculateFontSize(nameShift, styles.line.width),
+                    }}
+                  >
+                    {order.customer.lastName} -{' '}
+                  </Text>
+                  <Text
+                    style={[
+                      styles.bold,
+                      styles.capitalize,
+                      {
+                        fontSize: calculateFontSize(
+                          nameShift,
+                          styles.line.width
+                        ),
+                      },
+                    ]}
+                  >
+                    {order.company.shift === 'night'
+                      ? order.company.shift
+                      : categorizeLastName(order.customer.lastName)}
+                  </Text>
+                </View>
+                <Text
+                  style={[
+                    styles.line,
+                    {
+                      fontSize: calculateFontSize(
+                        order.restaurant.name,
+                        styles.line.width
+                      ),
+                    },
+                  ]}
+                >
+                  {order.restaurant.name}
                 </Text>
+                <Text
+                  style={[
+                    styles.line,
+                    styles.bold,
+                    {
+                      fontSize: calculateFontSize(
+                        order.item.name,
+                        styles.line.width
+                      ),
+                    },
+                  ]}
+                >
+                  {order.item.name}
+                </Text>
+                {(order.item.optionalAddons || order.item.requiredAddons) && (
+                  <Text
+                    style={[
+                      styles.line,
+                      {
+                        fontSize: calculateFontSize(addons, styles.line.width),
+                      },
+                    ]}
+                  >
+                    {order.item.requiredAddons} {order.item.optionalAddons}
+                  </Text>
+                )}
+                <Text>Optional addons</Text>
+                <Text>Required addons</Text>
+                <Text>Removed ingredients</Text>
+                {order.item.removedIngredients && (
+                  <Text
+                    style={[
+                      styles.line,
+                      {
+                        fontSize: calculateFontSize(
+                          order.item.removedIngredients,
+                          styles.line.width
+                        ),
+                      },
+                    ]}
+                  >
+                    {order.item.removedIngredients}
+                  </Text>
+                )}
               </View>
-              <Text style={styles.line}>{order.restaurant.name}</Text>
-              <Text style={[styles.line, styles.bold]}>{order.item.name}</Text>
-              {(order.item.optionalAddons || order.item.requiredAddons) && (
-                <Text style={styles.line}>
-                  {order.item.requiredAddons} {order.item.optionalAddons}
-                </Text>
-              )}
-              {order.item.removedIngredients && (
-                <Text style={styles.line}>{order.item.removedIngredients}</Text>
-              )}
             </View>
-          </View>
-        ))}
+          );
+        })}
       </Page>
     </Document>
   );
