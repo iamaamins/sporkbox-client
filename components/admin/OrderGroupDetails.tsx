783632d1d2ea4cd47162cf7939d1202b678a3240
Alamin Shaikh <hello@octib.com>
2024-08-08 10:26:31 +0600

    Updated order archive route

--- components/admin/OrderGroupDetails.tsx
+++ components/admin/OrderGroupDetails.tsx
@@ -103,7 +103,7 @@ export default function OrderGroupDetails({ isLoading, orderGroups }: Props) {
   async function updateStatus() {
     try {
       setIsUpdatingOrderStatus(true);
-      await axiosInstance.patch(`/orders/${orderId}/change-order-status`);
+      await axiosInstance.patch(`/orders/${orderId}/archive-order`);
       setAllUpcomingOrders((prevState) => ({
         ...prevState,
         data: prevState.data.filter((order) => order._id !== orderId),
