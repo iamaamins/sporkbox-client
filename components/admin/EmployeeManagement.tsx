bcb624ec1cc1a1da52f56781d939a0dc1be8d6d5
Alamin Shaikh <hello@octib.com>
2024-11-19 11:54:17 +0600

    Added weekly order stat

--- components/admin/EmployeeManagement.tsx
+++ components/admin/EmployeeManagement.tsx
@@ -2,10 +2,25 @@ import { useData } from '@context/Data';
 import styles from './EmployeeManagement.module.css';
 import { IoSearch } from 'react-icons/io5';
 import { FormEvent, useEffect, useState } from 'react';
-import { Customer } from 'types';
+import { CustomAxiosError, Customer } from 'types';
 import Link from 'next/link';
+import { axiosInstance, dateToText, showErrorAlert } from '@lib/utils';
+import { IoIosArrowRoundBack } from 'react-icons/io';
+import { IoIosArrowRoundForward } from 'react-icons/io';
+import { useUser } from '@context/User';
+import { useAlert } from '@context/Alert';
 
 export default function EmployeeManagement() {
+  return (
+    <section className={styles.container}>
+      <h2>Employee management</h2>
+      <Employees />
+      <WeeklyOrderStat />
+    </section>
+  );
+}
+
+function Employees() {
   const { customers } = useData();
   const [query, setQuery] = useState('');
   const [employees, setEmployees] = useState<Customer[]>([]);
@@ -23,60 +38,158 @@ export default function EmployeeManagement() {
     );
   }
 
+  // Get 10 customers
   useEffect(() => {
     if (customers.data.length > 0 && !query)
       setEmployees(customers.data.slice(0, 10));
   }, [customers, query]);
 
   return (
-    <section className={styles.container}>
-      <h2>
-        {customers.isLoading
-          ? 'Loading...'
-          : !customers.isLoading && customers.data.length === 0
-          ? 'No customers found'
-          : 'Employee management'}
-      </h2>
-      {employees.length > 0 && (
-        <>
-          <form onSubmit={handleSearch} className={styles.search}>
-            <input
-              type='text'
-              value={query}
-              placeholder='Search employees...'
-              onChange={(e) => setQuery(e.target.value.toLowerCase())}
-            />
-            <IoSearch className={styles.search_icon} size={20} />
-          </form>
-
-          <table>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Status</th>
-                <th>Company code</th>
+    <div className={styles.employees}>
+      <form onSubmit={handleSearch} className={styles.search}>
+        <input
+          type='text'
+          value={query}
+          placeholder='Search employees...'
+          onChange={(e) => setQuery(e.target.value.toLowerCase())}
+        />
+        <IoSearch className={styles.search_icon} size={20} />
+      </form>
+      {customers.isLoading ? (
+        <p className={styles.message}>Loading...</p>
+      ) : employees.length > 0 ? (
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Status</th>
+              <th>Company code</th>
+            </tr>
+          </thead>
+          <tbody>
+            {employees.map((employee) => (
+              <tr key={employee._id}>
+                <td className={styles.important}>
+                  <Link href={`/admin/dashboard/${employee._id}`}>
+                    <a>
+                      {employee.lastName} {employee.firstName}
+                    </a>
+                  </Link>
+                </td>
+                <td>{employee.email}</td>
+                <td>{employee.status}</td>
+                <td>{employee.companies[0].code}</td>
               </tr>
-            </thead>
-            <tbody>
-              {employees.map((employee) => (
-                <tr key={employee._id}>
-                  <td className={styles.important}>
-                    <Link href={`/admin/dashboard/${employee._id}`}>
-                      <a>
-                        {employee.lastName} {employee.firstName}
-                      </a>
-                    </Link>
-                  </td>
-                  <td>{employee.email}</td>
-                  <td>{employee.status}</td>
-                  <td>{employee.companies[0].code}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p className={styles.message}>No employee found</p>
       )}
-    </section>
+    </div>
+  );
+}
+
+type OrderStats = {
+  isLoading: boolean;
+  data: {
+    date: string;
+    count: number;
+  }[];
+};
+function WeeklyOrderStat() {
+  const today = new Date();
+  const { isAdmin } = useUser();
+  const { setAlerts } = useAlert();
+  const [currentDate, setCurrentDate] = useState(today);
+  const [orderStat, setOrderStat] = useState<OrderStats>({
+    isLoading: true,
+    data: [],
+  });
+
+  function goToPreviousWeek() {
+    setCurrentDate(
+      (prevState) =>
+        new Date(
+          prevState.getFullYear(),
+          prevState.getMonth(),
+          prevState.getDate() - 7
+        )
+    );
+  }
+
+  function goToNextWeek() {
+    setCurrentDate((prevState) => {
+      const nextDate = new Date(
+        prevState.getFullYear(),
+        prevState.getMonth(),
+        prevState.getDate() + 7
+      );
+      return nextDate > today ? prevState : nextDate;
+    });
+  }
+
+  const startDate = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth(),
+    currentDate.getDate() - 6
+  );
+  const endDate = currentDate;
+
+  // Get weekly order stat
+  useEffect(() => {
+    async function getWeeklyOrderStat(start: Date, end: Date) {
+      try {
+        setOrderStat((prevState) => ({ ...prevState, isLoading: true }));
+        const response = await axiosInstance.get(
+          `/orders/weekly-stat/${start}/${end}`
+        );
+        setOrderStat({ isLoading: false, data: response.data });
+      } catch (err) {
+        showErrorAlert(err as CustomAxiosError, setAlerts);
+      } finally {
+        setOrderStat((prevState) => ({ ...prevState, isLoading: false }));
+      }
+    }
+    if (isAdmin) getWeeklyOrderStat(startDate, endDate);
+  }, [isAdmin, currentDate]);
+
+  return (
+    <div className={styles.order_stat}>
+      <div className={styles.week_picker}>
+        <button onClick={goToPreviousWeek}>
+          <IoIosArrowRoundBack size={20} /> Prev
+        </button>
+        <p>
+          {dateToText(startDate)} ~ {dateToText(endDate)}
+        </p>
+        <button onClick={goToNextWeek}>
+          Next <IoIosArrowRoundForward size={20} />
+        </button>
+      </div>
+      {orderStat.isLoading ? (
+        <p className={styles.message}>Loading....</p>
+      ) : orderStat.data.length > 0 ? (
+        <table>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Customer count</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orderStat.data.map((el, index) => (
+              <tr key={index}>
+                <td>{dateToText(el.date)}</td>
+                <td>{el.count}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p className={styles.message}>No orders found</p>
+      )}
+    </div>
   );
 }
