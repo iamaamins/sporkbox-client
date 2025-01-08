302176a8f98a6486eac0c9bba0ce8b6a1fd0bb7d
Alamin Shaikh <hello@octib.com>
2025-01-08 12:15:58 +0600

    Cleaned up next config file

--- next.config.js
+++ next.config.js
@@ -12,31 +12,20 @@ const nextConfig = {
 
 module.exports = nextConfig;
 
-// Injected Content via Sentry Wizard Below
 const { withSentryConfig } = require('@sentry/nextjs');
 
 module.exports = withSentryConfig(
   module.exports,
   {
-    // Suppresses source map uploading logs during build
     silent: true,
     org: process.env.SENTRY_ORG,
     project: process.env.SENTRY_PROJECT,
   },
   {
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
     widenClientFileUpload: true,
-
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
     transpileClientSDK: true,
-
-    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
     tunnelRoute: '/monitoring',
-
-    // Hides source maps from generated client bundles
     hideSourceMaps: true,
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
     disableLogger: true,
   }
 );
