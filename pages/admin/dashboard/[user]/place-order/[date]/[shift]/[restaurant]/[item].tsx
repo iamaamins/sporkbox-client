5cea328b8615b6a4bf968fa4cda9ac96c35c4fb4
Alamin Shaikh <hello@octib.com>
2025-01-27 12:54:57 +0600

    Added admin check on the dashboard item page

--- pages/admin/dashboard/[user]/place-order/[date]/[shift]/[restaurant]/[item].tsx
+++ pages/admin/dashboard/[user]/place-order/[date]/[shift]/[restaurant]/[item].tsx
@@ -1,6 +1,18 @@
 import PlaceOrderItem from '@components/admin/dashboard/PlaceOrderItem';
+import { useUser } from '@context/User';
+import { checkUser } from '@lib/utils';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function ItemPage() {
+  const router = useRouter();
+
+  const { isUserLoading, isAdmin } = useUser();
+
+  useEffect(() => {
+    checkUser(isUserLoading, isAdmin, router);
+  }, [isUserLoading, isAdmin]);
+
   return (
     <main>
       <PlaceOrderItem />
