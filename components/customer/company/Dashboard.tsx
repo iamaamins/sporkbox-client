565b3d5ea827142b042e76b9ad533df91b93dada
Alamin Shaikh <hello@octib.com>
2025-01-29 12:56:17 +0600

    Added review stat on the company dashboard

--- components/customer/company/Dashboard.tsx
+++ components/customer/company/Dashboard.tsx
@@ -1,12 +1,14 @@
 import UserManagement from './UserManagement';
 import Pricing from './Pricing';
 import Restaurant from './Restaurant';
+import Review from './Review';
 
 export default function Dashboard() {
   return (
     <>
       <UserManagement />
       <Pricing />
+      <Review />
       <Restaurant />
     </>
   );
